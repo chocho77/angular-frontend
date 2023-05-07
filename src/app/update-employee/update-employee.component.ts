695a5c3c36b63961bb64dac4af67e27950c39588
chavdar.momchilov <chavmom@gmail.com>
2023-05-07 13:49:46 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/employee-list/employee-list.component.html 	modified:   src/app/employee-list/employee-list.component.ts 	modified:   src/app/update-employee/update-employee.component.html 	modified:   src/app/update-employee/update-employee.component.ts

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-update-employee',
   templateUrl: './update-employee.component.html',
   styleUrls: ['./update-employee.component.css']
 })
-export class UpdateEmployeeComponent {
+export class UpdateEmployeeComponent implements OnInit{
+
+  employee: Employee = new Employee();
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit() {
+    
+  }
+
+
+
 
 }
