0a33693a5d0b49e4d39ed3e56b009957d7fd2867
chavdar.momchilov <chavmom@gmail.com>
2023-05-07 15:24:17 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/employee.service.ts 	modified:   src/app/update-employee/update-employee.component.ts

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-employee',
@@ -8,12 +10,19 @@ import { Employee } from '../employee';
 })
 export class UpdateEmployeeComponent implements OnInit{
 
+  id!:number;
+
   employee: Employee = new Employee();
 
-  constructor(){}
+  constructor(private employeeService:EmployeeService,
+    private route: ActivatedRoute){}
 
   ngOnInit(): void {
-    
+    this.id = this.route.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    },
+    error => console.log(error));
   }
 
   onSubmit() {
